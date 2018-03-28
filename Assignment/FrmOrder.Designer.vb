73a0d49e66ca005070d0f37f3ae1b305df40394e
alexgcm97 <alex_gcm97@hotmail.com>
2018-03-28 18:39:03 +0800

    Add project files.

---
+++ Assignment/FrmOrder.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FrmOrder
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmOrder))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lvMenu = New System.Windows.Forms.ListView()
+        Me.btnBeverage = New System.Windows.Forms.Button()
+        Me.btnFood = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lvCart = New System.Windows.Forms.ListView()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lvMenu)
+        Me.GroupBox1.Controls.Add(Me.btnBeverage)
+        Me.GroupBox1.Controls.Add(Me.btnFood)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(334, 412)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Menu"
+        '
+        'lvMenu
+        '
+        Me.lvMenu.Location = New System.Drawing.Point(7, 62)
+        Me.lvMenu.Name = "lvMenu"
+        Me.lvMenu.Size = New System.Drawing.Size(321, 344)
+        Me.lvMenu.TabIndex = 2
+        Me.lvMenu.UseCompatibleStateImageBehavior = False
+        '
+        'btnBeverage
+        '
+        Me.btnBeverage.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBeverage.Location = New System.Drawing.Point(173, 21)
+        Me.btnBeverage.Name = "btnBeverage"
+        Me.btnBeverage.Size = New System.Drawing.Size(99, 34)
+        Me.btnBeverage.TabIndex = 1
+        Me.btnBeverage.Text = "Beverage"
+        Me.btnBeverage.UseVisualStyleBackColor = True
+        '
+        'btnFood
+        '
+        Me.btnFood.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnFood.Location = New System.Drawing.Point(46, 21)
+        Me.btnFood.Name = "btnFood"
+        Me.btnFood.Size = New System.Drawing.Size(99, 34)
+        Me.btnFood.TabIndex = 0
+        Me.btnFood.Text = "Food"
+        Me.btnFood.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lvCart)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(438, 13)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(367, 317)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Food Cart"
+        '
+        'lvCart
+        '
+        Me.lvCart.AllowDrop = True
+        Me.lvCart.Location = New System.Drawing.Point(7, 21)
+        Me.lvCart.Name = "lvCart"
+        Me.lvCart.Size = New System.Drawing.Size(354, 290)
+        Me.lvCart.TabIndex = 0
+        Me.lvCart.UseCompatibleStateImageBehavior = False
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSubmit.Location = New System.Drawing.Point(438, 337)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(232, 77)
+        Me.btnSubmit.TabIndex = 2
+        Me.btnSubmit.Text = "Submit Order"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.btnCancel.Location = New System.Drawing.Point(691, 379)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(114, 35)
+        Me.btnCancel.TabIndex = 3
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(691, 337)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(114, 36)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear Cart"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "9001.jpg")
+        '
+        'FrmOrder
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(832, 437)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "FrmOrder"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnBeverage As Button
+    Friend WithEvents btnFood As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents lvCart As ListView
+    Friend WithEvents lvMenu As ListView
+    Friend WithEvents ImageList1 As ImageList
+End Class
---
+++ Assignment/FrmOrder.vb
@@ -0,0 +1,44 @@
+﻿Public Class FrmOrder
+    Dim db As DataClasses1DataContext
+    Dim imageList As New ImageList
+
+    Private Sub FrmOrder_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        imageList.ColorDepth = ColorDepth.Depth32Bit
+        imageList.ImageSize = New Size(110, 110)
+        Dim files() As String = IO.Directory.GetFiles("../../Images/menu")
+        Dim imgStr As String
+        For i As Integer = 0 To files.Length - 1
+            imgStr = files(i)
+            imageList.Images.Add(New Bitmap(imgStr))
+            imageList.Images.SetKeyName(i, IO.Path.GetFileNameWithoutExtension(imgStr))
+        Next
+        db = New DataClasses1DataContext
+
+        Dim menuQuery = db.Menus
+
+        For Each menuItem In menuQuery
+            Dim item As New ListViewItem
+            item.Text = menuItem.menuName
+            item.ToolTipText = menuItem.menuName
+            For Each imgName As String In imageList.Images.Keys
+                If (imgName = menuItem.menuID) Then
+                    item.ImageIndex = imageList.Images.IndexOfKey(imgName)
+                End If
+            Next
+            lvMenu.Items.Add(item)
+        Next
+        lvMenu.LargeImageList = imageList
+    End Sub
+
+    Private Sub foodList_MouseClick(sender As Object, e As MouseEventArgs)
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lvCart.Clear()
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Me.Close()
+    End Sub
+End Class
